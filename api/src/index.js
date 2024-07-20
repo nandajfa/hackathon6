dedf8a89e5a5e8c8c29c8e5004e9daef6dd7cf2f
nandajfa <nanda.jfa@gmail.com>
2024-07-19 21:32:25 -0300

    Feat: create file index.js

---
+++ api/src/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const fastify = require('fastify')()
+const cors = require('@fastify/cors')
+const formbody = require('@fastify/formbody')
+const swagger = require('./plugins/swagger')
+const authRoutes = require('./routes/authRoutes')
+
+fastify.register(cors, { origin: true })
+fastify.register(formbody)
+fastify.register(swagger)
+fastify.register(authRoutes, { prefix: '/api' })
+
+const start = async () => {
+  try {
+    await fastify.listen({
+      port: 3003,
+      host: '0.0.0.0'
+    })
+    fastify.log.info(`Server is running at http://localhost:3003`)
+  } catch (err) {
+    fastify.log.error(err)
+    console.log(err.stack)
+    process.exit(1)
+  }
+}
+
+start()
