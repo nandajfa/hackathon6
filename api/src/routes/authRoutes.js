40f6fac54fa85a8a792c6aa2b3b002a4457fc097
nandajfa <nanda.jfa@gmail.com>
2024-07-19 21:33:09 -0300

    Feat: create routes

---
+++ api/src/routes/authRoutes.js
@@ -0,0 +1,33 @@
+const { register, login } = require('../controllers/authController')
+
+async function authRoutes(fastify, options) {
+  fastify.post('/register', {
+    schema: {
+      body: {
+        type: 'object',
+        required: ['email', 'password'],
+        properties: {
+          email: { type: 'string', format: 'email' },
+          password: { type: 'string', minLength: 6 }
+        }
+      }
+    },
+    handler: register
+  })
+
+  fastify.post('/login', {
+    schema: {
+      body: {
+        type: 'object',
+        required: ['email', 'password'],
+        properties: {
+          email: { type: 'string', format: 'email' },
+          password: { type: 'string', minLength: 6 }
+        }
+      }
+    },
+    handler: login
+  })
+}
+
+module.exports = authRoutes
