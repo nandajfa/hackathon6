271163e51377e8fd441d505c2560d018e58f11ae
nandajfa <nanda.jfa@gmail.com>
2024-07-24 16:20:19 -0300

    feat: add new route layout

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,6 +1,7 @@
 import Home from '../pages/Home'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
+import Dashboard from '../pages/Layout'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // import { PrivateRoute } from './privateRoute'
@@ -12,6 +13,7 @@ const AppRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<Register />} />
+        <Route path="/menu" element={<Dashboard />} />
 
         {/* <Route path="*" element={<Login />} /> */}
         {/* <Route
