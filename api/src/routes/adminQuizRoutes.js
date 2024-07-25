9c3d128aafe01fd5940a00788979d6f0182e4d5f
nandajfa <nanda.jfa@gmail.com>
2024-07-25 11:22:05 -0300

    refactor: update admin controller, service, and routes; modify table field and schema parameters

--- api/src/routes/adminQuizRoutes.js
+++ api/src/routes/adminQuizRoutes.js
@@ -4,28 +4,12 @@ const adminQuizController = require('../controllers/adminQuizController')
 
 async function adminQuizRoutes(fastify, options) {
   fastify.get('/admin/quizzes', {
-    schema: {
-      querystring: {
-        type: 'object',
-        required: ['table'],
-        properties: {
-          table: { type: 'string' }
-        }
-      }
-    },
     preHandler: [authenticate, authorizeAdmin],
     handler: adminQuizController.getAllQuizzes
   })
 
   fastify.get('/admin/quizzes/:id', {
     schema: {
-      querystring: {
-        type: 'object',
-        required: ['table'],
-        properties: {
-          table: { type: 'string' }
-        }
-      },
       params: {
         type: 'object',
         required: ['id'],
@@ -43,20 +27,20 @@ async function adminQuizRoutes(fastify, options) {
       body: {
         type: 'object',
         required: [
-          'table',
           'question',
           'option1',
           'option2',
           'option3',
-          'answer'
+          'answer',
+          'difficulty'
         ],
         properties: {
-          table: { type: 'string' },
           question: { type: 'string' },
           option1: { type: 'string' },
           option2: { type: 'string' },
           option3: { type: 'string' },
-          answer: { type: 'string' }
+          answer: { type: 'string' },
+          difficulty: { type: 'string' }
         }
       }
     },
@@ -69,27 +53,20 @@ async function adminQuizRoutes(fastify, options) {
       body: {
         type: 'object',
         required: [
-          'table',
           'question',
           'option1',
           'option2',
           'option3',
-          'answer'
+          'answer',
+          'difficulty'
         ],
         properties: {
-          table: { type: 'string' },
           question: { type: 'string' },
           option1: { type: 'string' },
           option2: { type: 'string' },
           option3: { type: 'string' },
-          answer: { type: 'string' }
-        }
-      },
-      params: {
-        type: 'object',
-        required: ['id'],
-        properties: {
-          id: { type: 'string' }
+          answer: { type: 'string' },
+          difficulty: { type: 'string' }
         }
       }
     },
@@ -99,13 +76,6 @@ async function adminQuizRoutes(fastify, options) {
 
   fastify.delete('/admin/quizzes/:id', {
     schema: {
-      body: {
-        type: 'object',
-        required: ['table'],
-        properties: {
-          table: { type: 'string' }
-        }
-      },
       params: {
         type: 'object',
         required: ['id'],
