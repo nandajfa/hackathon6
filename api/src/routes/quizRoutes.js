21e1457d5fa162f1dc0f47bb53a3bd3df2ce5cc9
nandajfa <nanda.jfa@gmail.com>
2024-07-27 14:31:03 -0300

    feat: add new route getQuestions

--- api/src/routes/quizRoutes.js
+++ api/src/routes/quizRoutes.js
@@ -1,8 +1,25 @@
 const quizController = require('../controllers/quizController')
+const authenticate = require('../middleware/authMiddleware')
 
 async function quizRoutes(fastify) {
   fastify.get('/quizzes', {
+    preHandler: authenticate,
     handler: quizController.getAllQuizzes
+  })
+
+  fastify.get('/quizzes/progress/:id', {
+    preHandler: authenticate,
+    handler: quizController.getUserProgress
+  })
+
+  fastify.get('/quizzes/questions/:id', {
+    preHandler: authenticate,
+    handler: quizController.getQuestions
+  })
+
+  fastify.post('/quizzes/submit', {
+    preHandler: authenticate,
+    handler: quizController.submitAnswers
   })
 }
 
