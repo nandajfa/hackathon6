22feb7fafc14b1553cad197f9f84449e52491850
nandajfa <nanda.jfa@gmail.com>
2024-07-27 14:32:16 -0300

    feat: add getQuestion service and controller

--- api/src/controllers/quizController.js
+++ api/src/controllers/quizController.js
@@ -9,6 +9,47 @@ async function getAllQuizzes(request, reply) {
   }
 }
 
+async function getUserProgress(request, reply) {
+  try {
+    const user_id = request.params.id
+
+    const result = await quizService.getUserProgress(user_id)
+    reply.send(result)
+  } catch (error) {
+    reply.code(500).send({ error: error.message })
+  }
+}
+
+async function getQuestions(request, reply) {
+  try {
+    const user_id = request.params.id
+    const questions = await quizService.getQuestions(user_id)
+    reply.send(questions)
+  } catch (error) {
+    reply.code(500).send({ error: error.message })
+  }
+}
+
+async function submitAnswers(request, reply) {
+  try {
+    const { questions, user_id, points, level } = request.body
+
+    const result = await quizService.checkAnswersAndUpdateUser(
+      questions,
+      user_id,
+      points,
+      level
+    )
+
+    reply.send(result)
+  } catch (error) {
+    reply.code(500).send({ error: error.message })
+  }
+}
+
 module.exports = {
-  getAllQuizzes
+  getAllQuizzes,
+  submitAnswers,
+  getUserProgress,
+  getQuestions
 }
--- api/src/services/quizService.js
+++ api/src/services/quizService.js
@@ -6,6 +6,82 @@ async function getAllQuizzes() {
   return data
 }
 
+async function checkAnswersAndUpdateUser(questions, user_id, points, level) {
+
+  const promises = questions.map(async questionObj => {
+    const question = questionObj.question
+    const { data, error: questionError } = await supabase
+      .from('quiz_questions')
+      .select('*')
+      .eq('question', question)
+
+    if (questionError) {
+      throw new Error(`Error fetching question: ${questionError.message}`)
+    }
+
+    for (const questionData of data) {
+      const question_id = questionData.id
+
+      const { error: insertError } = await supabase
+        .from('user_questions')
+        .insert({
+          user_id,
+          question_id
+        })
+
+      if (insertError) {
+        throw new Error(`Error inserting user_answer: ${insertError.message}`)
+      }
+    }
+  })
+
+  await Promise.all(promises)
+
+  const { error: updateError } = await supabase
+    .from('users')
+    .update({ points: points, level: level })
+    .eq('id', user_id)
+
+  if (updateError) {
+    throw new Error(`Error updating user: ${updateError.message}`)
+  }
+
+  return { correct: 'updated' }
+}
+
+async function getUserProgress(user_id) {
+  const { data: user, error: userError } = await supabase
+    .from('users')
+    .select('points, level')
+    .eq('id', user_id)
+    .single()
+
+  if (userError) {
+    throw new Error(`Error fetching user: ${userError.message}`)
+  }
+
+  return {
+    points: user.points,
+    level: user.level
+  }
+}
+
+async function getQuestions(user_id) {
+  const { data, error } = await supabase
+    .from('user_questions')
+    .select('question_id')
+    .eq('user_id', user_id)
+
+  if (error) {
+    throw new Error(`Error fetching user: ${userError.message}`)
+  }
+
+  return { data }
+}
+
 module.exports = {
-  getAllQuizzes
+  getAllQuizzes,
+  checkAnswersAndUpdateUser,
+  getUserProgress,
+  getQuestions
 }
