42ff3fe1c7fb877784b7a629eaf3ff32c7646604
nandajfa <nanda.jfa@gmail.com>
2024-07-28 20:41:00 -0300

    feat: add UserProvider to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { UserProvider } from './context/UserContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<App />)
+root.render(
+  <UserProvider>
+    <App />
+  </UserProvider>
+)
